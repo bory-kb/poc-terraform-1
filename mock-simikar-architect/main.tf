0b48680b6885d1c485083a5d64a0af8805c6c536
bory-kb <s.kubo0527@gmail.com>
2021-12-02 09:19:17 +0900

    deploy appservice

---
+++ mock-simikar-architect/main.tf
@@ -0,0 +1,56 @@
+# Configure the Azure provider
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 2.65"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "poc-${var.resource_name}"
+  location = var.location
+}
+
+
+resource "azurerm_app_service_plan" "example" {
+  name                = "plan-${var.resource_name}"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+
+  sku {
+    tier = "Basic"
+    size = "B1"
+  }
+
+}
+
+resource "azurerm_app_service" "example" {
+  name                = "app-${var.resource_name}"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  app_service_plan_id = azurerm_app_service_plan.example.id
+
+  site_config {
+    dotnet_framework_version = "v4.0"
+    scm_type                 = "None"
+    ftps_state = "ftpsOnly"
+  }
+
+  app_settings = {
+    "SOME_KEY" = "some-value"
+  }
+
+  connection_string {
+    name  = "Database"
+    type  = "SQLServer"
+    value = "Server=some-server.mydomain.com;Integrated Security=SSPI"
+  }
+}
---
+++ mock-simikar-architect/variables.tf
@@ -0,0 +1,8 @@
+
+variable "resource_name" {
+  default = "terraform-20211202"
+}
+
+variable "location" {
+  default = "JapanEast"
+}
